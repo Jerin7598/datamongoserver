be439ea07e3af5cdeb8e6439697135223da64897
unknown <jerins750@gmail.com>
2023-10-09 16:17:55 +0530

    good

---
+++ model.js
@@ -0,0 +1,26 @@
+// Schema for AddressBook
+const mongoose = require('mongoose');
+const addressSchema = mongoose.Schema({
+    name: {
+     type: String,
+     required: true
+    },
+    email: {
+     type: String,
+     required: true
+    },
+    phone: {
+     type: Number,
+     required: true
+    },
+    place: {
+     type: String,
+     required: true
+    }
+   })
+   //Creating the collection Address
+   const Address = mongoose.model('Address', addressSchema)
+
+   module.exports = Address;
+
+   mongodb+srv://jerins750:OqPPBFbTw56yD9xO@cluster0.6r9al8r.mongodb.net/?retryWrites=true&w=majority
---
+++ server.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+const PORT = 3000;
+
+
+
+
+
+// Connect to MongoDB
+
+mongoose.connect('mongodb+srv://jerins750:OqPPBFbTw56yD9xO@cluster0.6r9al8r.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+
+
+
+
+const User = mongoose.model('User', {
+  name: String,
+  email: String,
+  phone: String,
+  place: String
+});
+
+app.use(bodyParser.json());
+
+// Create a new user
+app.post('/users', async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Get all users
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Get a specific user by ID
+app.get('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Update a user by ID
+app.put('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Delete a user by ID
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.json({ message: 'User deleted successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
